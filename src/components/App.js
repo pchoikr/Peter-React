e50fa25f6944a6979f2dbc3018e324344602a256
PETER CHOI <pchoikr@gmail.com>
2019-07-10 21:38:57 +0900

    Add Sass module

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <section className="App-header">
         asdfasfsadfafsd
-      </header>
+      </section>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import './style.scss';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <h1>Heading test
+          <p>test</p>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
